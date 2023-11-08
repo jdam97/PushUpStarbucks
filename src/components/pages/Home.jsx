58f529902f590eec9ee4b6b7db322dd8d24a09b9
jdam97 <jonathandalvarez7@gmail.com>
2023-11-08 16:12:05 -0500

    feat: :sparkles: add structure

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,31 @@
+import "../../css/home.css"
+
+export const Home = ()=>{
+    return(
+        <div>
+            <header>
+                <div className="logo">
+                    <img src="/logo.png" alt="" />
+                </div>
+                <nav className="nav">
+                    <a href="">Inicio</a>
+                    <a href="">Menu</a>
+                    <a href="">Novedades</a>
+                    <a href="">Contacto</a>
+                </nav>
+            </header>
+            <div className="main-content">
+                <div className="info-product">
+
+                </div>
+                <div className="img-product">
+
+                    
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
